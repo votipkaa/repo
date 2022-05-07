3b605d7b62adb087c8d7e40c998fde1ed9fb0e67
votipkaa <105122781+votipkaa@users.noreply.github.com>
2022-05-07 13:15:24 -0400

    Update NFL.py

--- NFL.py
+++ NFL.py
@@ -6,7 +6,7 @@ import altair as alt
 st.title('NFL Project')
 st.markdown("## Bennie's First Data Science Project to Join the Coastal Elite 🏈")
 
-NFL_DATA = 'https://github.com/votipkaa/repo/blob/main/NFL%20Stats%202021-22%20Season.xlsx'
+NFL_DATA = 'NFL Stats 2021-22 Season.xlsx'
 points = st.slider("How many points scored?",0,900,10)
 
 @st.cache(persist=True)
